ccab8a616ebe22d79c737825352a8d01229a2318
Keethan <Keethan@gmail.com>
2021-02-08 01:57:41 +0530

    visualize alpaha to 1

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -62,7 +62,7 @@ def random_colors(N, bright=True):
     To get visually distinct colors, generate them in HSV space then
     convert to RGB.
     """
-    brightness = 1.0 if bright else 0.7
+    brightness = 1.0 if True else 0.7
     hsv = [(i / N, 1, brightness) for i in range(N)]
     colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
     random.shuffle(colors)
@@ -73,8 +73,7 @@ def apply_mask(image, mask, color, alpha=1):
     """Apply the given mask to the image.
     """
     for c in range(3):
-        image[:, :, c] = np.where(mask == 1,
-                                  color[c] * 255,
+        image[:, :, c] = np.where(mask == 1, color[c] * 255,
                                   #(1 - alpha) + alpha * color[c] * 255,
                                   image[:, :, c])
     return image
